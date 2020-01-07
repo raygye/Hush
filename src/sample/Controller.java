52018c4f8b1a3f47cbfc6c116bb85f6f35679426
yeG10 <garyye2007@gmail.com>
2020-01-07 18:04:46 -0500

    init

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+public class Controller {
+    int sel=-1;
+    public Button button;
+    public Button encode;
+    public Button decode;
+    public void sel1() {
+        sel = 0;
+    }
+    public void sel2() {
+        sel = 1;
+    }
+    public void submit() {
+
+    }
+}
