e3206ddfdb1470ee9856fafba1a647e846920565
yeG10 <garyye2007@gmail.com>
2020-01-09 22:35:28 -0500

    1.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,21 @@
 package sample;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.io.*;
+
 public class Controller {
-    int sel=-1;
+    int sel=0;
     public Button button;
     public Button encode;
     public Button decode;
     public Text select;
+    public TextField reveal;
+    public TextField read;
+    public PasswordField key;
     public void sel1() {
         decode.setStyle(null);
         System.out.println("Encode");
@@ -21,7 +28,34 @@ public class Controller {
         sel = 1;
         decode.setStyle("-fx-background-color: silver");
     }
-    public void submit() {
+    public void submit() throws IOException {
+        System.out.println("Submit");
+        String text = read.getText().replace(' ', 'λ');
+        String line;
+        OutputStream stdin = null;
+        InputStream stdout = null;
 
+        // execute and grab stdin/stdout
+        Process process = Runtime.getRuntime().exec("./src/sample/enigma.exe");
+        stdin = process.getOutputStream ();
+        stdout = process.getInputStream ();
+
+        // "write" the parms into stdin
+        line = sel + " " + text + " " + key.getText();
+        stdin.write(line.getBytes());
+
+        stdin.close();
+
+        // clean up if any output in stdout
+        BufferedReader readOut =
+                new BufferedReader (new InputStreamReader (stdout));
+        while ((line = readOut.readLine ()) != null) {
+            reveal.setVisible(true);
+            line = line.replace('λ', ' ');
+            reveal.setText(line);
+        }
+        readOut.close();
+
+        System.out.println("Success...");
     }
 }
